92da7548c5b807a63476a0db19ccc4eb29cd40d8
Boris Isac <boris.isac@arxi.ot>
2023-05-19 19:10:58 +0100

    has been created function door*hp and it print in car_form.fielf.function_result. has been createg field greating witch contain hello + Driver

--- boris_odoo/models/models.py
+++ boris_odoo/models/models.py
@@ -8,11 +8,17 @@ class Car ( models.Model ) :
 	name = fields.Char ( string = 'Name' )
 	hps_car = fields.Integer ( string = "HP's Car" )
 	doors_car = fields.Integer ( string = "Doors's Car" )
+	total_spead=fields.Integer(string = 'Function result', compute='say_hello')
 
 	driver_id = fields.Many2one ( 'res.partner', string = 'Driver' )
 	parking_id = fields.Many2one ( 'parking.parking', string = 'Parking Position' )
 	feature_ids = fields.Many2many ( 'car.feature', string = 'Features' )
+	greating_label = fields.Char ( string = 'Hello', related="driver_id.name")
 
+	def say_hello( self ):
+		print('******************************************************')
+		print(self.greating_label)
+		self.total_spead = self.doors_car * self.hps_car
 
 class Parking ( models.Model ) :
 	_name = 'parking.parking'
