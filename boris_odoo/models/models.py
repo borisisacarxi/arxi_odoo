d2a6cfef4e06c0452cec1430f2a10d9b1628bd99
Boris Isac <boris.isac@arxi.ot>
2023-05-19 10:08:00 +0100

    has been created a summenu parking witch belong to menu car, has been created parking.xml for parking form, tree, action. has been added new module parking and fields many2one

--- boris_odoo/models/models.py
+++ boris_odoo/models/models.py
@@ -1,4 +1,3 @@
-
 from odoo import models, fields
 
 
@@ -6,6 +5,17 @@ class Car ( models.Model ) :
 	_name = 'car.car'
 	_description = 'Cars'
 
-	name = fields.Char ( string = 'Model' )
+	name = fields.Char ( string = 'Name' )
 	hps_car = fields.Integer ( string = "HP's Car" )
 	doors_car = fields.Integer ( string = "Doors's Car" )
+
+	driver_id = fields.Many2one ( 'res.partner', string = 'Driver' )
+	parking_id = fields.Many2one ( 'parking.parking', string = 'Parking Position' )
+
+
+class Parking ( models.Model ) :
+	_name = 'parking.parking'
+	_description = 'parking'
+
+	name = fields.Char ( string = "Parking's position" )
+	car_ids = fields.One2many ( 'car.car', 'parking_id' )
