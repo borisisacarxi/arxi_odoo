0908540f66e486e2d230442225af598ef596f821
Boris Isac <boris.isac@arxi.ot>
2023-05-24 12:15:10 +0100

    had been instaled new app FLEET. i have created  few cars to someone(in my case administrator). when o arived to contacts upstares you can see smart button witch print me haw many cars i have in use and after a click it show me list(tree) of my cars

--- boris_odoo/models/res_partner.py
+++ boris_odoo/models/res_partner.py
@@ -1,8 +1,25 @@
 from odoo import models, api, fields
 
 
-class Res_partner ( models.Model ) :
+class ResPartner ( models.Model ) :
 	_inherit = "res.partner"
 
 	message = fields.Char ( string = "message" )
 	other_message = fields.Char ( string = 'other message' )
+
+	car_count = fields.Integer ( string = 'Count', compute = 'get_car_number' )
+
+	def get_car_number ( self ) :
+		for rec in self :
+			rec.car_count = self.env['fleet.vehicle'].search_count([('driver_id', '=', self.id)])
+
+	def get_cars ( self ) :
+		self.ensure_one ()
+		return {
+			'type' : 'ir.actions.act_window',
+			'name' : 'Cars',
+			'view_mode' : 'tree',
+			'res_model' : 'fleet.vehicle',
+			'domain' : [('driver_id', '=', self.id)],
+			'context' : "{'create':False}"
+		}
